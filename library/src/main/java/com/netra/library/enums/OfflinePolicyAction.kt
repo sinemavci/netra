1ca0a4133dcad154fa9ca0f98f47fcdd7708a888
sinemavci <sinem.avci@ekinokssoftware.com>
2026-05-05 17:37:55 +0300

    refactor: add identifiers to polict actions

--- library/src/main/java/com/netra/library/enums/OfflinePolicyAction.kt
+++ library/src/main/java/com/netra/library/enums/OfflinePolicyAction.kt
@@ -1,8 +1,22 @@
 package com.netra.library.enums
 
-sealed class OfflinePolicyAction {
-    object QUEUE : OfflinePolicyAction()
-    object USE_CACHE : OfflinePolicyAction()
-    data class RETRY(val retries: Int) : OfflinePolicyAction()
-    object THROW_ERROR : OfflinePolicyAction()
+sealed class OfflinePolicyAction(val identifier: String) {
+    object QUEUE : OfflinePolicyAction("QUEUE")
+    object USE_CACHE : OfflinePolicyAction("USE_CACHE")
+    data class RETRY(val retries: Int) : OfflinePolicyAction("RETRY")
+    object THROW_ERROR : OfflinePolicyAction("THROW_ERROR")
+
+    companion object {
+        fun fromIdentifier(identifier: String, retries: Int?): OfflinePolicyAction {
+            return when (identifier) {
+                "QUEUE" -> QUEUE
+                "USE_CACHE" -> USE_CACHE
+                "RETRY" -> RETRY(retries ?: 1)
+                "THROW_ERROR" -> THROW_ERROR
+                else -> {
+                    throw Throwable("Unknown offline policy: $identifier")
+                }
+            }
+        }
+    }
 }
--- library/src/main/java/com/netra/library/enums/SlowNetworkPolicyAction.kt
+++ library/src/main/java/com/netra/library/enums/SlowNetworkPolicyAction.kt
@@ -1,7 +1,20 @@
 package com.netra.library.enums
 
-sealed class SlowNetworkPolicyAction {
-    data class WAIT(val delay: Long) : SlowNetworkPolicyAction()
-    data class TIMEOUT(val timeout: Long) : SlowNetworkPolicyAction()
-    data object USE_CACHE : SlowNetworkPolicyAction()
+sealed class SlowNetworkPolicyAction(val identifier: String) {
+    data class WAIT(val delay: Long) : SlowNetworkPolicyAction("WAIT")
+    data class TIMEOUT(val timeout: Long) : SlowNetworkPolicyAction("TIMEOUT")
+    object USE_CACHE : SlowNetworkPolicyAction("USE_CACHE")
+
+    companion object {
+        fun fromIdentifier(identifier: String, delay: Long?, timeout: Long?): SlowNetworkPolicyAction {
+            return when (identifier) {
+                "WAIT" -> WAIT(delay ?: 1)
+                "USE_CACHE" -> USE_CACHE
+                "TIMEOUT" -> TIMEOUT(timeout ?: 1)
+                else -> {
+                    throw Throwable("Unknown offline policy: $identifier")
+                }
+            }
+        }
+    }
 }
