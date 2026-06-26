d9fed6fb334acaf4c0f20adb60470833a1ae85d6
sinemavci <sinem.avci@ekinokssoftware.com>
2026-06-26 18:43:51 +0300

    refactor: retryInterval added

--- library/src/main/java/com/netra/library/enums/OfflinePolicyAction.kt
+++ library/src/main/java/com/netra/library/enums/OfflinePolicyAction.kt
@@ -3,7 +3,7 @@ package com.netra.library.enums
 sealed class OfflinePolicyAction(val identifier: String) {
     object QUEUE : OfflinePolicyAction("QUEUE")
     object USE_CACHE : OfflinePolicyAction("USE_CACHE")
-    data class RETRY(val retries: Int) : OfflinePolicyAction("RETRY")
+    data class RETRY(val retries: Int, val retryInterval: Long? = 2000) : OfflinePolicyAction("RETRY")
     object THROW_ERROR : OfflinePolicyAction("THROW_ERROR")
 
     companion object {
