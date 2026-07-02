7150851d76c075d53414d7bdb1973fa53be7d434
sinemavci <sinem.avci@ekinokssoftware.com>
2026-07-02 17:30:21 +0300

    refactor: remove all companion object from client and move this to netra provider

--- library/src/main/java/com/netra/library/observers/INetraObserver.kt
+++ library/src/main/java/com/netra/library/observers/INetraObserver.kt
@@ -1,11 +1,11 @@
 package com.netra.library.observers
 
 interface INetraObserver {
-    fun onNetworkChanged(event: NetworkEvent)
+    fun onNetworkChanged(event: NetworkEvent) {}
 
-    fun onCacheChanged(event: CacheEvent)
+    fun onCacheChanged(event: CacheEvent) {}
 
-    fun onRequestChanged(event: RequestEvent)
+    fun onRequestChanged(event: RequestEvent) {}
 
-    fun onQueueChanged(event: QueueEvent)
+    fun onQueueChanged(event: QueueEvent) {}
 }
